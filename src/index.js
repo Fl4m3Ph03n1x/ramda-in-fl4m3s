d2ef65e58c079b87da585ac1678280b9b7853443
Pedro Martins <palmelapedro@gmail.com>
2018-05-10 11:53:43 +0200

    Added new functiuns, commenting code.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,135 @@
 const { isEmpty, pipe, all, equals, isNil, assoc, curry, is, prop, dissoc, last, append, reduce, map, identity, allPass, filter, complement } = require("ramda");
 
+/**
+ * @description
+ * Complement of <code>isEmpty</code>. Returns <code>false</code> if the given value is its type's empty value; <code>true</code> otherwise.
+ * 
+ * @param   {*}         val The value to test.
+ * @returns {Boolean}   <code>false</code> if the object is empty, <code>true</code> otherwise.
+ * 
+ * @see     http://ramdajs.com/docs/#isEmpty
+ */
 const notEmpty = complement( isEmpty );
 
-const allTrue = all( equals( true ) );
-
+/**
+ * @description
+ * Complement of <code>isNil</code>. Returns <code>true</code> if the given value is **not** <code>null</code> or <code>undefined</code>, <code>false</code> otherwise.
+ * 
+ * @param   {*}         val The value to test.
+ * @returns {Boolean}   <code>false</code> if the object is <code>null</code> or <code>undefined</code>, <code>false</code> otherwise.
+ * 
+ * @see     http://ramdajs.com/docs/#isNil
+ */
 const notNil = complement( isNil );
 
-const isFunction = is( Function );
+/**
+ * @description
+ * Composition of <code>all</code> and <code>equals</code>. 
+ * Returns <code>true</code> if all the values in the given list are equal to the value to test against.
+ * 
+ * @param   {*}         val     The value to test equality against.
+ * @param   {List}      list    The list we wish to check.
+ * @returns {Boolean}   <code>true</code> if all the values in the list equal <code>val</code>, <code>false</code> otherwise.
+ * 
+ * @see     http://ramdajs.com/docs/#all
+ * @see     http://ramdajs.com/docs/#equal
+ */
+const allEqual = curry(
+    ( value, list ) => all( equals( value ), list )
+);
 
+/**
+ * @description
+ * Returns <code>true</code> of the given value is a Function, <code>false</code> otherwise.
+ * 
+ * @param   {*}         val The value to test if it is a Function.
+ * @returns {Boolean}   <code>true</code> if <code>val</code> is a Function, <code>false</code> otherwise.
+ * 
+ * @see http://ramdajs.com/docs/#is
+ */
+const isFunction    =   is( Function );
+
+/**
+ * @description
+ * Returns <code>true</code> of the given value is a Number, <code>false</code> otherwise.
+ * 
+ * @param   {*}         val The value to test if it is a Number.
+ * @returns {Boolean}   <code>true</code> if <code>val</code> is a Number, <code>false</code> otherwise.
+ * 
+ * @see http://ramdajs.com/docs/#is
+ */
+const isNumber      =   is( Number );
+
+/**
+ * @description
+ * Returns <code>true</code> of the given value is a String, <code>false</code> otherwise.
+ * 
+ * @param   {*}         val The value to test if it is a String.
+ * @returns {Boolean}   <code>true</code> if <code>val</code> is a String, <code>false</code> otherwise.
+ * 
+ * @see http://ramdajs.com/docs/#is
+ */
+const isString      =   is( String );
+
+/**
+ * @description
+ * Returns <code>true</code> of the given value is an Array, <code>false</code> otherwise.
+ * 
+ * @param   {*}         val The value to test if it is an Array.
+ * @returns {Boolean}   <code>true</code> if <code>val</code> is an Array, <code>false</code> otherwise.
+ * 
+ * @see http://ramdajs.com/docs/#is
+ */
+const isArray       =   is( Array );
+
+/**
+ * @description
+ * Returns <code>true</code> of the given value is a Map, <code>false</code> otherwise.
+ * 
+ * @param   {*}         val The value to test if it is a Map.
+ * @returns {Boolean}   <code>true</code> if <code>val</code> is a Map, <code>false</code> otherwise.
+ * 
+ * @see http://ramdajs.com/docs/#is
+ */
+const isMap         =   is( Map );
+
+/**
+ * @description
+ * Returns a copy of the array with all falsey values removed.
+ * 
+ * @param   {List}  list    The list to compact.
+ * @returns {List}  The new compacted list.
+ */
+const compact = filter(
+    allPass([identity, notEmpty])
+);
+
+/**
+ * @description
+ * Makes a shallow clone of the target object, setting or overriding the specified property with the result of the given function over the target object. 
+ * Note that this copies and flattens prototype properties onto the new object as well. All non-primitive properties are copied by reference.
+ * 
+ * @param   {String}    prop            The property name to set.  
+ * @param   {Function}  fn              The transformation function to apply to <code>obj</code>. The result of the evaluation of this function will be the value of <code>prop</code>.
+ * @param   {Object}    obj             The object to clone.
+ * @returns {Object}    A new object equivalent to <code>obj</code> but with <code>prop</code> changed to have the result of <code>fn</code> as a value.
+ * 
+ * @see     http://ramdajs.com/docs/#assoc
+ */
 const assocTrans = curry(
     ( prop, transformation, object ) => assoc( prop, transformation( object ), object )
 );
 
+/**
+ * @description
+ * Makes a shallow clone of the target object, renaming the given property.
+ * Note that this copies and flattens prototype properties onto the new object as well. All non-primitive properties are copied by reference.
+ * 
+ * @param   {String}    oldPropName The property to be renamed.  
+ * @param   {String}    newPropName The new name of the property.
+ * @param   {Object}    obj         The object to clone.
+ * @returns {Object}    A new object equivalent to <code>obj</code> but with the given property renamed.
+ */
 const renameProp = curry(
     ( oldPropName, newPropName, object ) => pipe(
         assocTrans( newPropName, prop(oldPropName) ),
@@ -19,13 +137,23 @@ const renameProp = curry(
     )( object )
 );
 
+/**
+ * @description
+ * Applies <code>fn</code> sequentially to each element of <code>list</code>. 
+ * If <code>fn</code> is async and returns a Promise, then each Promise will be executed only when the previous Promise has resolved.
+ * If <code>fn</code> does not return a Promise, then its result is lifted into one.
+ * 
+ * @param   {Function}  fn      The function to apply to each element of <code>list</code>.
+ * @param   {List}      list    The list to be iterated over sequentially.
+ * @returns {List}      A list of Promises with the results of applying <code>fn</code> to <code>list</code>.
+ */
 const mapToSequentialPromises = curry(
     ( transformationFn, dataArray ) =>
         reduce(
             ( array, currData ) => {
 
-                const next = isEmpty( array )                               ?
-                    Promise.resolve( transformationFn( currData ) )                            :
+                const next = isEmpty( array )                           ?
+                    Promise.resolve( transformationFn( currData ) )     :
                     last(array).then( () => transformationFn( currData ) );
 
                 return append( next, array );
@@ -37,24 +165,33 @@ const mapToSequentialPromises = curry(
 
 const waitAll = requests => Promise.all( requests );
 
-const applyAsyncFn = asyncFn => pipe(
-    map( asyncFn ),
-    waitAll
-);
-
-const compact = filter(
-    allPass([identity, notEmpty])
+/**
+ * @description
+ * Applies <code>fn</code> concurrently to each element of <code>list</code>. 
+ * Unlike <code>mapToSequentialPromises</code> the execution of Promise N+1 will not wait for the resolution of Promise N. 
+ * Finishes once all the Promises are done via use of Promise.all. 
+ * 
+ * @param   {Function}  fn      The function to apply to each element of <code>list</code>.
+ * @param   {List}      list    The list to be iterated over concurrently.
+ * @returns A single Promise containing the new list.
+ */
+const mapAsyncFn = curry(
+    ( asyncFn, list ) => Promise.all( map( asyncFn, list ) )
 );
 
 module.exports = Object.freeze( {
     notEmpty,
-    allTrue,
+    allEqual,
     notNil,
     assocTrans,
     isFunction,
+    isNumber,
+    isMap,
+    isString,
+    isArray,
     renameProp,
     mapToSequentialPromises,
     waitAll,
-    applyAsyncFn,
+    mapAsyncFn,
     compact
 } );
