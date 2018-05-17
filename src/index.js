898b69b14035d0ffca1d15a78fa7a11cf377e57e
Pedro Miguel Pereira Serrano Martins <pedro@pedro-bunsenDev>
2018-05-17 22:15:08 +0200

    updating documentation

--- src/index.js
+++ src/index.js
@@ -11,13 +11,12 @@ const { isEmpty, pipe, all, equals, isNil, assoc, curry, is, prop, dissoc, last,
  * @see     http://ramdajs.com/docs/#isEmpty
  * 
  * @example
- * const { notEmpty } = require("ramda-in-fl4m3s");
- * notEmpty([1, 2, 3]);   //=> true
- * notEmpty([]);          //=> false
- * notEmpty('');          //=> false
- * notEmpty(null);        //=> true
- * notEmpty({});          //=> false
- * notEmpty({length: 0}); //=> true
+ * notEmpty( [1, 2, 3] );   //=> true
+ * notEmpty( [] );          //=> false
+ * notEmpty( "" );          //=> false
+ * notEmpty( null );        //=> true
+ * notEmpty( {} );          //=> false
+ * notEmpty( {length: 0} ); //=> true
  */
 const notEmpty = complement( isEmpty );
 
@@ -32,11 +31,10 @@ const notEmpty = complement( isEmpty );
  * @see     http://ramdajs.com/docs/#isNil
  * 
  * @example
- * const { notNil } = require("ramda-in-fl4m3s");
- * notNil(null); //=> false
- * notNil(undefined); //=> false
- * notNil(0); //=> true
- * notNil([]); //=> true
+ * notNil( null );          //=> false
+ * notNil( undefined );     //=> false
+ * notNil( 0 );             //=> true
+ * notNil( [] );            //=> true
  */
 const notNil = complement( isNil );
 
@@ -53,6 +51,10 @@ const notNil = complement( isNil );
  * @see     http://ramdajs.com/docs/#all
  * @see     http://ramdajs.com/docs/#equal
  * 
+ * @example
+ * allEqual( 1, [1, 1, 1] );              //=> true
+ * allEqual( 1, [] );                     //=> true
+ * allEqual( true, [true, false, true] ); //=> false
  */
 const allEqual = curry(
     ( value, list ) => all( equals( value ), list )
@@ -67,6 +69,13 @@ const allEqual = curry(
  * @returns {Boolean}   <code>true</code> if <code>val</code> is a Function, <code>false</code> otherwise.
  * 
  * @see http://ramdajs.com/docs/#is
+ * 
+ * @example
+ * isFunction( console.log );   //=> true
+ * isFunction( 1 );             //=> false
+ * isFunction( "" );            //=> false
+ * isFunction( [] );            //=> false
+ * isFunction( new Map() );     //=> false
  */
 const isFunction    =   is( Function );
 
@@ -79,6 +88,13 @@ const isFunction    =   is( Function );
  * @returns {Boolean}   <code>true</code> if <code>val</code> is a Number, <code>false</code> otherwise.
  * 
  * @see http://ramdajs.com/docs/#is
+ * 
+ * @example
+ * isNumber( console.log ); //=> false
+ * isNumber( 1 );           //=> true
+ * isNumber( "" );          //=> false
+ * isNumber( [] );          //=> false
+ * isNumber( new Map() );   //=> false
  */
 const isNumber      =   is( Number );
 
@@ -91,6 +107,13 @@ const isNumber      =   is( Number );
  * @returns {Boolean}   <code>true</code> if <code>val</code> is a String, <code>false</code> otherwise.
  * 
  * @see http://ramdajs.com/docs/#is
+ * 
+ * @example
+ * isString( console.log ); //=> false
+ * isString( 1 );           //=> false
+ * isString( "" );          //=> true
+ * isString( [] );          //=> false
+ * isString( new Map() );   //=> false
  */
 const isString      =   is( String );
 
@@ -103,6 +126,13 @@ const isString      =   is( String );
  * @returns {Boolean}   <code>true</code> if <code>val</code> is an Array, <code>false</code> otherwise.
  * 
  * @see http://ramdajs.com/docs/#is
+ * 
+ * @example
+ * isArray( console.log ); //=> false
+ * isArray( 1 );           //=> false
+ * isArray( "" );          //=> false
+ * isArray( [] );          //=> true
+ * isArray( new Map() );   //=> false
  */
 const isArray       =   is( Array );
 
@@ -115,16 +145,26 @@ const isArray       =   is( Array );
  * @returns {Boolean}   <code>true</code> if <code>val</code> is a Map, <code>false</code> otherwise.
  * 
  * @see http://ramdajs.com/docs/#is
+ * 
+ * @example
+ * isMap( console.log ); //=> false
+ * isMap( 1 );           //=> false
+ * isMap( "" );          //=> false
+ * isMap( [] );          //=> false
+ * isMap( new Map() );   //=> true
  */
 const isMap         =   is( Map );
 
 /**
  * @function    compact
  * @description
- * Returns a copy of the array with all falsey values removed.
+ * Returns a copy of the array with all falsey and empty values removed.
  * 
  * @param   {List}  list    The list to compact.
  * @returns {List}  The new compacted list.
+ * 
+ * @example
+ * compact( [0, 1, false, 2, '', 3, [], {}] );  //=> [1, 2, 3]
  */
 const compact = filter(
     allPass([identity, notEmpty])
@@ -230,6 +270,9 @@ const seedlessReduce = curry(
  * @returns {Promise}   A single promise containing the list of results if every promise in <code>pList</code> was fulfilled.
  * 
  * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise/all
+ * 
+ * @example
+ * promiseAll( [ Promise.resolve(1), Promise.resolve(2) ] ).then( console.log );    //=> [1, 2]
  */
 const promiseAll = pList => Promise.all( pList );
 
